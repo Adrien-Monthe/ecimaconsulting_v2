0ea1c21da6cb0a2e1b4016805417a1cce5e5253b
Steve Adrien <montheadrien@gmail.com>
2023-08-10 17:15:27 +0100

    first commit

---
+++ resources/views/layouts/frontend/meta.blade.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
+<meta name="keywords" content="Startup, news, journal, blog, company, corporate, Business, Multi-uses, HTML, Clean, Modern, Creative" />
+<meta name="author" content="Shreethemes" />
+<meta name="email" content="adrien.monthe@ecimaconsulting.com" />
+<meta name="website" content="https://ecimaconsulting.com/blog" />
+<meta name="Version" content="v1.0.0" />
